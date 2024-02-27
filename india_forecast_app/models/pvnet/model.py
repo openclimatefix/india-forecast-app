6f9f4808cbbc3f70725afdf372a14a5b3679266a
peterdudfield <peter.dudfield@hotmail.com>
2024-02-27 16:47:18 +0000

    upgrade ocf-datapipes, and bump pvnet git version

--- india_forecast_app/models/pvnet/model.py
+++ india_forecast_app/models/pvnet/model.py
@@ -31,7 +31,7 @@ from .utils import populate_data_config_sources, reset_stale_nwp_timestamps, wor
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 WIND_MODEL_NAME = os.getenv("WIND_MODEL_NAME", default="openclimatefix/windnet_india")
 WIND_MODEL_VERSION = os.getenv("WIND_MODEL_VERSION",
-                               default="c6af802823edc5e87b22df680b41b0dcdb4869e1")
+                               default="e5e482fe86928d5571bfda049903fcc0a414e6c9")
 
 PV_MODEL_NAME = os.getenv("PV_MODEL_NAME", default="openclimatefix/pvnet_india")
 PV_MODEL_VERSION = os.getenv("PV_MODEL_VERSION",
