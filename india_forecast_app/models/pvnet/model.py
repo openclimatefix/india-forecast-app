d930c43d5720f18b06010b85664202b77ad1a455
Jacob Bieker <jacob@bieker.tech>
2024-04-04 17:15:52 +0100

    Update model.py

--- india_forecast_app/models/pvnet/model.py
+++ india_forecast_app/models/pvnet/model.py
@@ -133,7 +133,7 @@ class PVNetModel:
                 if generation_da != 0 and not np.isnan(generation_da):
                     # Feather in the difference between this value and the next forecasted values
                     for idx, smooth_value in enumerate([0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]):
-                        values_df["forecast_power_kw"][idx] -= (values_df["forecast_power_kw"][idx] \
+                        values_df["forecast_power_kw"][idx] -= (values_df["forecast_power_kw"][idx]\
                         - generation_da) * smooth_value 
             # Smooth with a 1 hour rolling window
             # Only smooth the wind else we introduce too much of a lag in the solar 
