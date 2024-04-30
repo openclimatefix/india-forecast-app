4653409c9ad2d2df7a54b848fcc11d8ddcc477ea
peterdudfield <peter.dudfield@hotmail.com>
2024-04-30 12:49:57 +0100

    isort

--- india_forecast_app/models/pvnet/model.py
+++ india_forecast_app/models/pvnet/model.py
@@ -16,9 +16,9 @@ from ocf_datapipes.training.pvnet_site import construct_sliced_data_pipeline as 
 from ocf_datapipes.training.windnet import DictDatasetIterDataPipe, split_dataset_dict_dp
 from ocf_datapipes.training.windnet import construct_sliced_data_pipeline as wind_base_pipeline
 from ocf_datapipes.utils import Location
-from pvsite_datamodel.sqlmodels import SiteAssetType
 from pvnet.data.utils import batch_to_tensor, copy_batch_to_device
 from pvnet.models.base_model import BaseModel as PVNetBaseModel
+from pvsite_datamodel.sqlmodels import SiteAssetType
 from torch.utils.data import DataLoader
 from torch.utils.data.datapipes.iter import IterableWrapper
 
