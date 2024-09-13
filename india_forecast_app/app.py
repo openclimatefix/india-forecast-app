a7e5859e6e8a77d7f280c1b2c257ab25ca721e78
peterdudfield <peter.dudfield@hotmail.com>
2024-09-13 11:09:53 +0100

    fix typo

--- india_forecast_app/app.py
+++ india_forecast_app/app.py
@@ -310,7 +310,7 @@ def app(timestamp: dt.datetime | None, write_to_db: bool=False, log_level: str="
                 models[asset_type] = get_model(asset_type, timestamp, generation_data)
                 log.info(f"{asset_type} model loaded")
 
-        sucessful_runs = 0
+        successful_runs = 0
         for site in sites:
             # 3. Run model for each site
             site_id = site.site_uuid
@@ -342,10 +342,10 @@ def app(timestamp: dt.datetime | None, write_to_db: bool=False, log_level: str="
                 )
                 sucessful_runs += 1
 
-        log.info(f"Completed forecasts for {sucessful_runs} runs for {len(sites)} sites")
-        if sucessful_runs == len(sites):
+        log.info(f"Completed forecasts for {successful_runs} runs for {len(sites)} sites")
+        if successful_runs == len(sites):
             log.info("All forecasts completed successfully")
-        elif 0 < sucessful_runs < len(sites):
+        elif 0 < successful_runs < len(sites):
             raise Exception("Some forecasts failed")
         else:
             raise Exception("All forecasts failed")
