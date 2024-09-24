f378b21099989a18ed176ef1ec720253e1b22209
peterdudfield <peter.dudfield@hotmail.com>
2024-09-24 13:31:29 +0100

    lint

--- tests/models/test_pydantic_models.py
+++ tests/models/test_pydantic_models.py
@@ -1,11 +1,14 @@
+""" Test for getting all ml models"""
 from india_forecast_app.models.pydantic_models import get_all_models
 
 
 def test_get_all_models():
+    """Test for getting all models"""
     models = get_all_models()
     assert len(models.models) == 4
 
 
 def test_get_all_models_client():
+    """Test for getting all models for a specific client"""
     models = get_all_models(client_abbreviation="dummy")
     assert len(models.models) == 1
