6da1b514b603ae270a8e03025a37c99fcd90aa12
peterdudfield <peter.dudfield@hotmail.com>
2024-12-20 12:54:49 +0000

    update tests

--- tests/models/test_pydantic_models.py
+++ tests/models/test_pydantic_models.py
@@ -5,7 +5,7 @@ from india_forecast_app.models.pydantic_models import get_all_models
 def test_get_all_models():
     """Test for getting all models"""
     models = get_all_models()
-    assert len(models.models) == 9
+    assert len(models.models) == 10
 
 
 def test_get_all_models_client():
